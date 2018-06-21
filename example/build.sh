34266a37cebcb8d0f0e8d3f7cc73443ee9fc8071
河野　晋策 <shinsaku_kono@r.recruit.co.jp>
2018-06-21 19:08:33 +0900

    feat(install flow): :shirt:

--- example/build.sh
+++ example/build.sh
@@ -2,4 +2,4 @@
 
 rm -rf ./node_modules/react-native-group-thumbnail
 mkdir ./node_modules/react-native-group-thumbnail
-rsync -v -a --exclude example --exclude node_modules ../ ./node_modules/react-native-group-thumbnail/
+rsync -v -a --exclude example --exclude node_modules --exclude .git --exclude flow-typed --exclude .DS_Store --exclude yarn-error.log ../ ./node_modules/react-native-group-thumbnail/
--- src/GroupThumbnail.js
+++ src/GroupThumbnail.js
@@ -1,9 +1,6 @@
+// @flow
 import React, { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
-
-// type Props = {
-//     containerStyle: ViewPropTypes.style,
-// }
 
 const styles = StyleSheet.create({
   container: { backgroundColor: 'red' },
