1d0c784a60ff282d5a9b0670fa497c8b2a919f01
lapis-zero09 <lapis-zero09@klis.tsukuba.ac.jp>
2018-06-25 03:33:23 +0900

    fix(refactor css) :shirt:

--- src/ThumbnailBlock.js
+++ src/ThumbnailBlock.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react'
-import { Image, View } from 'react-native'
+import { Image } from 'react-native'
 import styled, { css } from 'styled-components'
 
 type Props = {
@@ -8,174 +8,219 @@ type Props = {
   options: Array<string>,
 }
 
+const Center = css`
+  justify-content: center;
+  align-items: center;
+`
+
 const InitialTextFormat = css`
   color: #fff;
   font-weight: bold;
   align-self: center;
 `
 
-const InitialThumbSingleText = styled.Text`
-  margin-right: ${(p) => (p.right ? '3px' : '0.5px')};
-  margin-left: ${(p) => (p.left ? '3px' : '0.5px')};
-  margin-top: 50%;
-  font-size: 14px;
-  ${InitialTextFormat};
+// const ThumbnailText = styled.Text`
+//   margin-right: ${(p) => (p.right ? '3px' : '0.5px')};
+//   margin-left: ${(p) => (p.left ? '3px' : '0.5px')};
+//   font-size: 14px;
+//   ${InitialTextFormat};
+// `
+
+// const ThumbnailText = styled.Text`
+//   margin-bottom: ${(p) => (p.bottom ? '5px' : '1px')};
+//   margin-top: ${(p) => (p.top ? '5px' : '1px')};
+//   margin-right: ${(p) => (p.right ? '5px' : '1px')};
+//   margin-left: ${(p) => (p.left ? '5px' : '1px')};
+//   font-size: 12px;
+//   ${InitialTextFormat};
+// `
+
+// const InitialThumbText = styled.Text`
+//   margin: 3px;
+//   font-size: 25px;
+//   ${InitialTextFormat};
+//   border-radius: 18px;
+// `
+
+  // ${(props) => !props.single && css`margin-right: ${(props) => (props.right ? props.marginMaxSize : props.marginMinSize)}px`};
+  // ${(props) => !props.single && css`margin-left: ${(props) => (props.left ? props.marginMaxSize : props.marginMinSize)}px`};
+
+const ThumbnailText = styled.Text.attrs({
+  size: (props) => (props.small ? 12 : (props.large ? 16 : 14)),
+  marginMaxSize: (props) => (props.single ? 5 : 3),
+  marginMinSize: (props) => (props.single ? 1 : 0.5),
+})`
+  color: #fff;
+  align-self: center;
+  font-weight: bold;
+  font-size: ${(props) => (props.only ? props.size+10 :(props.single ? props.size+2 : props.size))}px;
+  margin-bottom: ${(props) => (props.bottom ? props.marginMaxSize : props.marginMinSize)}px;
+  margin-top: ${(props) => (props.top ? props.marginMaxSize : props.marginMinSize)}px;
+  margin-right: ${(props) => (props.right ? props.marginMaxSize : props.marginMinSize)}px;
+  margin-left: ${(props) => (props.left ? props.marginMaxSize : props.marginMinSize)}px;
 `
 
-const InitialThumbMultipleText = styled.Text`
-  margin-bottom: ${(p) => (p.bottom ? '5px' : '1px')};
-  margin-top: ${(p) => (p.top ? '5px' : '1px')};
-  margin-right: ${(p) => (p.right ? '5px' : '1px')};
-  margin-left: ${(p) => (p.left ? '5px' : '1px')};
-  font-size: 12px;
-  ${InitialTextFormat};
-`
+// const SingleTextView = styled.View`
+//   background-color: rgba(0, 0, 0, 0);
+//   width: 18px;
+//   height: 36px;
+// `
 
-const InitialThumbSingle = styled(Image)`
-  width: 18px;
-  height: 36px;
-  margin-right: ${(p) => (p.right ? '0px' : '1px')};
-  margin-left: ${(p) => (p.left ? '-1px' : '1px')};
-`
+// const MultipleTextView = styled.View`
+//   background-color: rgba(0, 0, 0, 0);
+//   width: 18px;
+//   height: 18px;
+//   justify-content: space-evenly;
+//   align-items: center;
+// `
 
-const InitialThumbMultiple = styled(Image)`
-  width: 18px;
-  height: 18px;
-  margin-top: ${(p) => (p.top ? '0px' : '1px')};
-  margin-bottom: ${(p) => (p.bottom ? '-1px' : '1px')};
-  margin-right: ${(p) => (p.right ? '0px' : '1px')};
-  margin-left: ${(p) => (p.left ? '-1px' : '1px')};
-`
+// const InitialThumb = styled.View`
+//   background-color: rgb(153, 153, 153);
+//   justify-content: center;
+//   align-items: center;
+//   height: 36px;
+//   width: 36px;
+//   border-radius: 18px;
+// `
 
-const SingleTextView = styled.View`
-  background-color: rgba(0, 0, 0, 0);
-  width: 18px;
-  height: 36px;
-`
-
-const MultipleTextView = styled.View`
-  background-color: rgba(0, 0, 0, 0);
-  width: 18px;
-  height: 18px;
-  justify-content: space-evenly;
-  align-items: center;
-`
-
-const InitialThumbText = styled.Text`
-  margin: 3px;
-  font-size: 25px;
-  ${InitialTextFormat};
-  border-radius: 18px;
-`
-
-const InitialThumb = styled.View`
+const ThumbnailView = styled.View.attrs({
+  size: (props) => (props.small ? 36 : (props.large ? 80 : 56)),
+})`
   background-color: rgb(153, 153, 153);
-  justify-content: center;
+  width: ${props => props.only ? props.size : props.size/2}px;
+  height: ${props => props.only ? props.size : props.size/2}px;
   align-items: center;
-  height: 36px;
-  width: 36px;
-  border-radius: 18px;
+  justify-content: center;
 `
 
-const Thumb = styled(Image)`
-  justify-content: center;
-  align-items: center;
-  height: 36px;
-  width: 36px;
-  border-radius: 18px;
+// const Thumb = styled(Image)`
+//   justify-content: center;
+//   align-items: center;
+//   height: 36px;
+//   width: 36px;
+//   border-radius: 18px;
+// `
+
+// const InitialThumbSingle = styled(Image)`
+//   width: 18px;
+//   height: 36px;
+//   margin-right: ${(p) => (p.right ? '0px' : '1px')};
+//   margin-left: ${(p) => (p.left ? '-1px' : '1px')};
+// `
+
+// const InitialThumbMultiple = styled(Image)`
+//   width: 18px;
+//   height: 18px;
+//   margin-top: ${(p) => (p.top ? '0px' : '1px')};
+//   margin-bottom: ${(p) => (p.bottom ? '-1px' : '1px')};
+//   margin-right: ${(p) => (p.right ? '0px' : '1px')};
+//   margin-left: ${(p) => (p.left ? '-1px' : '1px')};
+// `
+
+const Thumbnail = styled(Image).attrs({
+  size: (props) => (props.small ? 36 : props.large ? 80 : 56),
+})`
+  width: ${props => props.single ? props.size : props.size/2}px;
+  height: ${props => props.single ? props.size : props.size/2}px;
+  margin-top: ${(p) => (p.top ? 0 : 1)}px;
+  margin-bottom: ${(p) => (p.bottom ? -1 : 1)}px;
+  margin-right: ${(p) => (p.right ? 0 : 1)}px;
+  margin-left: ${(p) => (p.left ? -1 : 1)}px;
+  ${props => props.only && Center};
 `
 
-const ThumbnailBlock = ({ item, options }: Props) => {
-  if (item.length === 1) {
+const ThumbnailBlock = (props: any) => {
+  if (props.item.length === 1) {
     // initial icon
-    if (options.includes('left')) {
+    if (props.options.includes('left')) {
       // left content
-      if (options.includes('top')) {
+      if (props.options.includes('top')) {
         // left top content
         return (
-          <MultipleTextView>
-            <InitialThumbMultipleText top left>
-              {item}
-            </InitialThumbMultipleText>
-          </MultipleTextView>
+          <ThumbnailView {...props}>
+            <ThumbnailText {...props} top left>
+              {props.item}
+            </ThumbnailText>
+          </ThumbnailView>
         )
-      } else if (options.includes('bottom')) {
+      } else if (props.options.includes('bottom')) {
         // left bottom
         return (
-          <MultipleTextView>
-            <InitialThumbMultipleText bottom left>
-              {item}
-            </InitialThumbMultipleText>
-          </MultipleTextView>
+          <ThumbnailView {...props}>
+            <ThumbnailText {...props} bottom left>
+              {props.item}
+            </ThumbnailText>
+          </ThumbnailView>
         )
       }
       // left single content
       return (
-        <SingleTextView>
-          <InitialThumbSingleText left>{item}</InitialThumbSingleText>
-        </SingleTextView>
+        <ThumbnailView {...props} single>
+          <ThumbnailText {...props} single left>{props.item}</ThumbnailText>
+        </ThumbnailView>
       )
-    } else if (options.includes('right')) {
+    } else if (props.options.includes('right')) {
       // right content
-      if (options.includes('top')) {
+      if (props.options.includes('top')) {
         // right top content
         return (
-          <MultipleTextView>
-            <InitialThumbMultipleText top right>
-              {item}
-            </InitialThumbMultipleText>
-          </MultipleTextView>
+          <ThumbnailView {...props}>
+            <ThumbnailText {...props} top right>
+              {props.item}
+            </ThumbnailText>
+          </ThumbnailView>
         )
-      } else if (options.includes('bottom')) {
+      } else if (props.options.includes('bottom')) {
         // right bottom content
         return (
-          <MultipleTextView>
-            <InitialThumbMultipleText bottom right>
-              {item}
-            </InitialThumbMultipleText>
-          </MultipleTextView>
+          <ThumbnailView {...props}>
+            <ThumbnailText {...props} bottom right>
+              {props.item}
+            </ThumbnailText>
+          </ThumbnailView>
         )
       }
       // right single content
       return (
-        <SingleTextView>
-          <InitialThumbSingleText right>{item}</InitialThumbSingleText>
-        </SingleTextView>
+        <ThumbnailView {...props} single>
+          <ThumbnailText {...props} single right>{props.item}</ThumbnailText>
+        </ThumbnailView>
       )
     }
     // initialThumb only
     return (
-      <InitialThumb>
-        <InitialThumbText>{item}</InitialThumbText>
-      </InitialThumb>
+      <ThumbnailView {...props} only>
+        <ThumbnailText {...props} only>{props.item}</ThumbnailText>
+      </ThumbnailView>
     )
   }
   // thumbnail content
-  if (options.includes('left')) {
+  if (props.options.includes('left')) {
     // left content
-    if (options.includes('top')) {
+    if (props.options.includes('top')) {
       // top left content
-      return <InitialThumbMultiple left top source={{ uri: item }} />
-    } else if (options.includes('bottom')) {
+      return <Thumbnail {...props} left top source={{ uri: props.item }} />
+    } else if (props.options.includes('bottom')) {
       // bottom left content
-      return <InitialThumbMultiple left bottom source={{ uri: item }} />
+      return <Thumbnail {...props} left bottom source={{ uri: props.item }} />
     }
     // left single content
-    return <InitialThumbSingle left source={{ uri: item }} />
-  } else if (options.includes('right')) {
+    return <Thumbnail {...props} single left source={{ uri: props.item }} />
+  } else if (props.options.includes('right')) {
     // right content
-    if (options.includes('top')) {
+    if (props.options.includes('top')) {
       // top right content
-      return <InitialThumbMultiple right top source={{ uri: item }} />
-    } else if (options.includes('bottom')) {
+      return <Thumbnail {...props} right top source={{ uri: props.item }} />
+    } else if (props.options.includes('bottom')) {
       // bottom right content
-      return <InitialThumbMultiple right bottom source={{ uri: item }} />
+      return <Thumbnail {...props} right bottom source={{ uri: props.item }} />
     }
     // right single Image
-    return <InitialThumbSingle right source={{ uri: item }} />
+    return <Thumbnail {...props} single right source={{ uri: props.item }} />
   }
   // thumbnail only
-  return <Thumb source={{ uri: item }} />
+  console.log(props)
+  return (<Thumbnail single only {...props} source={{ uri: props.item }} />)
 }
 
 export default ThumbnailBlock
