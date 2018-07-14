6dab7053f86fefe0849eb1e1a915eb3d6aab8333
lapis-zero09 <lapis-zero09@klis.tsukuba.ac.jp>
2018-07-14 23:18:12 +0900

    TypeError: Cannot read property 'attrs' of undefined

--- __tests__/GroupThumbnail.ios.js
+++ __tests__/GroupThumbnail.ios.js
@@ -4,11 +4,15 @@ import renderer from 'react-test-renderer'
 
 import GroupThumbnail from '../src/GroupThumbnail'
 
-const members = [{ name: '河野', iconPath: '' }]
+// const members = [{ name: '河野', iconPath: '' }]
 
-// const members = [{ name: 'lapis-zero09', iconPath: 'https://avatars3.githubusercontent.com/u/12763048?s=460&v=4' }]
+const members = [{ name: 'lapis-zero09', iconPath: 'https://avatars3.githubusercontent.com/u/12763048?s=460&v=4' }]
 
-it('renders correctly', () => {
-  const tree = renderer.create(<GroupThumbnail small members={members} />).toJSON()
+describe('Button Block', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+    .create(<GroupThumbnail small members={members} />)
+    .toJSON()
   expect(tree).toMatchSnapshot()
+  })
 })
--- example/build.sh
+++ example/build.sh
@@ -2,4 +2,4 @@
 
 rm -rf ./node_modules/react-native-group-thumbnail
 mkdir ./node_modules/react-native-group-thumbnail
-rsync -v -a --exclude example --exclude node_modules --exclude .git --exclude flow-typed --exclude .DS_Store --exclude yarn-error.log ../ ./node_modules/react-native-group-thumbnail/
+rsync -v -a --exclude Example --exclude node_modules --exclude .git --exclude flow-typed --exclude .DS_Store --exclude yarn-error.log ../ ./node_modules/react-native-group-thumbnail/
